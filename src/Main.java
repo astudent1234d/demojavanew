680994c3a9d00796da3738516bd874d64cd6dee4
user <test@>
2021-11-07 01:04:56 +0300

    Добавлен файл .gitignore, текст программы Miles

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticket_price = 5000;
+        int cost_of_a_mile = 20;
+        int miles = ticket_price / cost_of_a_mile;
+        System.out.println(miles);
+        }
+}
